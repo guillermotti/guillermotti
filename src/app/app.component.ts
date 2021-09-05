b7012b9a74af89b31e6f557b59a50bb723f1f253
Guille Vigil <guillermovigilr@gmail.com>
2021-09-05 22:40:23 +0200

    Last changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isDark = false;
   textButton = 'Heck, bring me dark';
-  arrowSvg = 'M11.8 13.2l-1.4-2.83-6.83 3.94L.2 8.46l6.83-3.94-1.76-2.64L7.78.43l8.34 2.73-1.8 8.6-2.52 1.45zm1.68-1.65l1.72-7.86-7.66-2.43 2.02 3.1-6.75 3.9 2.24 3.88 6.75-3.9 1.68 3.3z';
 
   changeMode() {
     this.isDark = !this.isDark;
